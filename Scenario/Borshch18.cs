ea42d6cf78440a872802e2f397b593cd5590b246
OrnarasUS <ornaras.us@gmail.com>
2025-03-11 12:19:20 +0400

    Добавлено группирование блюд и форматирование в консоли

--- Scenario/Borshch18.cs
+++ Scenario/Borshch18.cs
@@ -6,14 +6,36 @@ namespace OrnaUBot.Scenario;
 
 internal static class Borshch18
 {
-    public static string[] WhitelistCategories { get; } = ["48", "49", "50", "51"];
+    public static string[] WhitelistCategories { get; } = 
+        ["48", "49", "50", "51", "53", "57"];
+    public static Dictionary<int, string> Categories { get; } = 
+        new Dictionary<int, string>
+    {
+        { 48, "Салаты" },
+        { 49, "Супы" },
+        { 50, "Горячие блюда" },
+        { 51, "Гарнир" },
+        { 53, "Хлеб" },
+        { 57, "Соусы" }
+    };
 
     public static async Task<string> SendOrder()
     {
         var menu = await PullMenu();
         for (var i = 0; i < menu.Count; i++)
+        {
+            if (i == 0 || menu[i-1].Category != menu[i].Category)
+            {
+                (Console.BackgroundColor, Console.ForegroundColor) = 
+                    (Console.ForegroundColor, Console.BackgroundColor);
+                Console.WriteLine($"{Categories[menu[i].Category]}:");
+            }
             Console.WriteLine($"{i + 1}) {menu[i]}");
-        var selectedIndex = Console.ReadLine()!.Split(',').Select(t => int.Parse(t) - 1).ToArray();
+        }
+        Console.ResetColor();
+        Console.WriteLine();
+        Console.Write("Напиши номер блюд через пробел: ");
+        var selectedIndex = Console.ReadLine()!.Split(' ').Where(t=>!string.IsNullOrWhiteSpace(t)).Select(t => int.Parse(t) - 1).ToArray();
         var res = new StringBuilder();
         var sum = 0;
         foreach (var index in selectedIndex)
@@ -21,7 +43,7 @@ internal static class Borshch18
             res.AppendLine(menu[index].Name);
             sum += menu[index].Price;
         }
-        res.Append($"<tg-spoiler>{sum} руб.</tg-spoiler>");
+        res.Append($"<tg-spoiler>{sum}₽</tg-spoiler>");
 
         var msg = await TelegramBot.SendMessage(Configuration.Telegram_Me_Id, res.ToString());
 
@@ -53,9 +75,12 @@ internal static class Borshch18
             {
                 Name = i!["name"]!.GetValue<string>(),
                 Price = int.Parse(i!["values"]![0]!["price"]!.GetValue<string>()),
-                Mass = i!["values"]![0]!["mass"]!.GetValue<string>()
+                Mass = i!["values"]![0]!["mass"]!.GetValue<string>(),
+                Category = int.Parse(i!["categoryID"]!.GetValue<string>())
             });
         }
+
+        res.Sort((p,n) => p.Category.CompareTo(n.Category));
 
         return res;
     }
@@ -65,7 +90,8 @@ internal static class Borshch18
         public string Name { get; set; }
         public int Price { get; set; }
         public string Mass { get; set; }
+        public int Category { get; set; }
 
-        public override string ToString() => $"{Name} [{Price} \\ {Mass}]";
+        public override string ToString() => $"{Name} [{Price} руб \\ {Mass}]";
     }
 }
